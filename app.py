dd4e134af9a3c262debb929409506962196f2ec8
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-03-06 11:21:45 -0500

    Uso de render_template y envio de datos

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app= Flask(__name__)
 
+
+@app.route("/")
+def index():
+    data={
+        "titulo":"Pagina de libros",
+        "encabezado":"Bienvenido"
+    }
+    return render_template("index.html",data=data)
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
