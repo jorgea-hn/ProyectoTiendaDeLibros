b58349d154cf3558a19e02629072dc64ea17e28b
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-03-06 11:38:56 -0500

    Herencia de plantillas

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def index():
     }
     return render_template("index.html",data=data)
 
+@app.route("/contacto")
+def contacto():
+    data={
+        "titulo":"contacto",
+        "encabezado":"Bienvenido"
+    }
+    return render_template("contacto.html",data=data)
+
 if __name__=="__main__":
     app.run(debug=True)
